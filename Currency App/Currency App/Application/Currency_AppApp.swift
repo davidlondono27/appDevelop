00ac014911a23d1a63e702782731ecb59a12bc6a
David Santiago Londono Giraldo <david.londono@mercadolibre.com.co>
2023-03-11 12:22:30 -0500

    Create base project and apply MVVM + Clean Architecture

---
+++ Currency App/Currency App/Application/Currency_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_AppApp.swift
+//  Currency App
+//
+//  Created by David Santiago Londono Giraldo on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Currency_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Currency App/Currency App/Tests/Currency AppUITests/Currency_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Currency_AppUITestsLaunchTests.swift
+//  Currency AppUITests
+//
+//  Created by David Santiago Londono Giraldo on 11/03/23.
+//
+
+import XCTest
+
+final class Currency_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
