3f1a0a9b2029445f6ce65e5553c53cc0ca25bd8c
David Santiago Londono Giraldo <david.londono@mercadolibre.com.co>
2023-04-01 13:44:30 -0500

    New changes

--- Currency App/Currency App/Presentation/UserDetails/View/ContentView.swift
+++ Currency App/Currency App/Presentation/UserDetails/View/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @StateObject var calculator : CalculatorViewModel = CalculatorViewModel()
+    @State var name = "David"
+    @State var inputUser = ""
+    @State var query : QueryModel = QueryModel(from: "EUR", to: "USD", amount: 1)
+    @State var result = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Spacer()
+                Image(systemName: "dollarsign.circle.fill")
+                    .imageScale(.large)
+                    .padding(.bottom, 5)
+                Text("\(ConstantsText_ES.welcome)!\n Estás consultando \(query.amount) \(query.from) en \(query.to).")
+                    .multilineTextAlignment(.center)
+                
+                Text("Resultado: \(result)")
+                    .padding(.vertical, 20)
+                Spacer()
+                Button {
+                    requestValue(query: query, result: &result)
+                } label: {
+                    generalButton(buttonImage: "network", buttonText: "Consultar!")
+                }
+                Spacer()
+            }
+    }
+    
+    func requestValue(query: QueryModel, result: inout String) {
+        let calculator = CalculatorViewModel()
+        calculator.queryItems = query
+        calculator.loadData()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            print("Desde request: \(calculator.calculatorResponse)")
         }
-        .padding()
+        result = String(calculator.calculatorResponse.result)
     }
 }
 
