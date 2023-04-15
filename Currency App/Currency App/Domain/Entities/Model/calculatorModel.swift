da2fc605fd5127b6afe4dfb70c6808ef108a5086
danyvalenz <jesusvalenzuelag25@gmail.com>
2023-04-15 12:28:19 -0700

    Se quitan espacios de la capa model

--- Currency App/Currency App/Domain/Entities/Model/calculatorModel.swift
+++ Currency App/Currency App/Domain/Entities/Model/calculatorModel.swift
@@ -16,27 +16,23 @@ struct CalculatorModel: Decodable {
     
     enum CodingKeys: String, CodingKey {
 
-      case success = "success"
-      case query   = "query"
-      case info    = "info"
-      case date    = "date"
-      case result  = "result"
+      case success
+      case query
+      case info
+      case date
+      case result 
     
     }
     init(from decoder: Decoder) throws {
       let values = try decoder.container(keyedBy: CodingKeys.self)
 
-        success = try values.decode(Bool.self   , forKey: .success )
-        query   = try values.decode(QueryModel.self  , forKey: .query   )
-        info    = try values.decode(Info.self   , forKey: .info    )
-        date    = try values.decode(String.self   , forKey: .date    )
-        result  = try values.decode(Double.self , forKey: .result  )
+        success = try values.decode(Bool.self, forKey: .success)
+        query   = try values.decode(QueryModel.self, forKey: .query)
+        info    = try values.decode(Info.self, forKey: .info)
+        date    = try values.decode(String.self, forKey: .date)
+        result  = try values.decode(Double.self, forKey: .result)
    
     }
-
-   
-    
-   
 }
 
 // MARK: - Query
