d7601653d627dbd50c7e3b208ec721a014a9efed
danyvalenz <jesusvalenzuelag25@gmail.com>
2023-04-29 13:35:40 -0700

    correcciones de texto a constant_text
    se realizan los cambios de inicializar la variable a 0 y de  remplazar todas las strings por constantes

--- Currency App/Currency App/Presentation/UserDetails/View/ContentView.swift
+++ Currency App/Currency App/Presentation/UserDetails/View/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @StateObject var calculatorViewModel : currencyViewModel = currencyViewModel()
     @StateObject var symbolViewModel : symbolsViewModel = symbolsViewModel()
     @State var result = ""
-    @State var amount = 1
+    @State var amount = 0
     @State var toggleSymbols = false
     @State var origenCurrency = ""
     @State var destinoCurrency = ""
@@ -39,7 +39,7 @@ struct ContentView: View {
                     Text(ConstantsText.emptyCurrencies)
                     Toggle(isOn: $toggleSymbols) { // -> Este Toggle cambia entre simbolos y países
                         Image(systemName: "arrow.counterclockwise.circle.fill")
-                        Text("Sólo simbolos")
+                        Text(ConstantsText.soloSimbolos)
                     }
                     .frame(width: 200)
                 }.onTapGesture {
@@ -48,7 +48,7 @@ struct ContentView: View {
                 
                 List {
                     Section {
-                        Picker("Origen", selection: $origenCurrency) {
+                        Picker(ConstantsText.origen, selection: $origenCurrency) {
                             ForEach(symbolViewModel.symbolsList.keys.sorted(), id: \.self){ symbol in
                                 if toggleSymbols {
                                     Text(symbol)
@@ -58,10 +58,10 @@ struct ContentView: View {
                             }
                         }
                     } header: {
-                        Text("Origen")
+                        Text(ConstantsText.origen)
                     }
                     Section {
-                        Picker("Destino", selection: $destinoCurrency) {
+                        Picker(ConstantsText.destino, selection: $destinoCurrency) {
                             ForEach(symbolViewModel.symbolsList.keys.sorted(), id: \.self){ symbol in
                                 if toggleSymbols {
                                     Text(symbol)
@@ -71,13 +71,13 @@ struct ContentView: View {
                             }
                         }
                     } header: {
-                        Text("Destino")
+                        Text(ConstantsText.destino)
                     }
                 }.animation(.easeInOut, value: toggleSymbols) // -> Esta animación suaviza un poco el cambio visual entre simbolos y países
                     .listStyle(.inset) // -> Este es el estilo de lista, pueden cambiarlo por ejemplo a .automatic para que vean la diferencia
                 Text("\(ConstantsText.currentSearch) \(query.amount) \(origenCurrency) \(ConstantsText.fromAndTo) \(destinoCurrency).")
                     .multilineTextAlignment(.center)
-                TextField("ingrese cantidad", value: $amount, formatter: formatter)
+                TextField(ConstantsText.cantidadIngresar, value: $amount, formatter: formatter)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding().keyboardType(.numberPad)
                     
@@ -86,7 +86,7 @@ struct ContentView: View {
                     calculatorViewModel.loadData(query: query)
                 } label: {
                     //TODO: Cambiar por General Button
-                    Text("Consultar")
+                    Text(ConstantsText.consultar)
                         .padding(.vertical, 10)
                 }
                 if calculatorViewModel.result == 0.0 {
